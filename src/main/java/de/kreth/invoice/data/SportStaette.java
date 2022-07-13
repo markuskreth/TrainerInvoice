2d69b2f9182a8e4f34a86c5f2749fe58c55edfa5
Markus Kreth <markus.kreth@web.de>
2022-07-13 23:25:43 +0200

    Fixed gitignore

---
+++ src/main/java/de/kreth/invoice/data/SportStaette.java
@@ -0,0 +1,69 @@
+package de.kreth.invoice.data;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SPORTSTAETTE")
+public class SportStaette extends BaseEntity {
+
+    private static final long serialVersionUID = 9087707740670720452L;
+    @Column(nullable = false, name = "user_id")
+    private long userId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    public long getUserId() {
+	return userId;
+    }
+
+    public void setUserId(long userId) {
+	this.userId = userId;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    @Override
+    protected String getMediumRepresentation() {
+	return name;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result + Objects.hash(name, userId);
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) {
+	    return true;
+	}
+	if (!super.equals(obj)) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	SportStaette other = (SportStaette) obj;
+	return Objects.equals(name, other.name) && userId == other.userId;
+    }
+
+    @Override
+    public String toString() {
+	return "SportStaette [getId()=" + getId() + ", userId=" + userId + ", name=" + name + "]";
+    }
+
+}
